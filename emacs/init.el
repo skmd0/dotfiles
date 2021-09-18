6a38e4fa3660d0a1d54983bc6c20fd4c35347d8b
domen@skamlic.com <domen@skamlic.com>
2021-09-18 15:04:22 +0200

    simplify company mode config

--- emacs/init.el
+++ emacs/init.el
@@ -241,10 +241,15 @@
 (use-package lsp-ui)
 
 ;; auto-completion while you type
-(use-package company)
-(add-hook 'after-init-hook 'global-company-mode)
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 1)
+(use-package company
+  :after lsp-mode
+  :hook (prog-mode . company-mode)
+  :bind
+  (:map company-active-map ("<tab>" . company-complete-selection))
+  (:map lsp-mode-map ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0))
 
 ;; code snippet system
 (use-package yasnippet)
