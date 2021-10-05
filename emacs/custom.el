79ac8208059bc33a4ef9361ab84c33e411503307
domen@skamlic.com <domen@skamlic.com>
2021-10-05 09:41:21 +0200

    update custom auto-generated

--- emacs/custom.el
+++ emacs/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dired-single yasnippet which-key vterm use-package treemacs-projectile rainbow-delimiters org-roam neotree magit lsp-ui lsp-treemacs lsp-ivy ivy-rich ivy-posframe go-mode general evil-snipe evil-leader evil-collection doom-themes doom-modeline counsel-projectile company centaur-tabs)))
+   '(dired-hide-dotfiles dired-single yasnippet which-key vterm use-package treemacs-projectile rainbow-delimiters org-roam neotree magit lsp-ui lsp-treemacs lsp-ivy ivy-rich ivy-posframe go-mode general evil-snipe evil-leader evil-collection doom-themes doom-modeline counsel-projectile company centaur-tabs)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
