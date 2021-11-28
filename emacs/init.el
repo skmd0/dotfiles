733be1d660e0cd841cd6c8f0743f065d631008de
Domen Skamlic <domen@skamlic.com>
2021-11-28 12:37:53 +0100

    remove orgmode elpa repository

--- emacs/init.el
+++ emacs/init.el
@@ -76,7 +76,6 @@
 ;; initialize package sources
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
 			 ("elpa" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 (unless package-archive-contents (package-refresh-contents))
