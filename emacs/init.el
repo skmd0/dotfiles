2c0e96dc8b7cf25b990b7bfe0ec3ce200aed7eff
Domen Skamlic <domen@skamlic.com>
2021-11-29 06:17:33 +0100

    remove doom themes

--- emacs/init.el
+++ emacs/init.el
@@ -117,25 +117,6 @@
   :config
   (org-roam-db-autosync-mode))
 
-;; doom-themes color schemes
-;; (use-package doom-themes
-;;   :config
-;;   ;; Global settings (defaults)
-;;   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-;;         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-;;   (load-theme 'doom-nord t)
-;;   ;; Enable flashing mode-line on errors
-;;   (doom-themes-visual-bell-config)
-;;   ;; Corrects (and improves) org-mode's native fontification.
-;;   (doom-themes-org-config))
-
-;; (use-package all-the-icons
-;;   :if (display-graphic-p))
-
-;; doom modeline
-;; (use-package doom-modeline
-;;   :init (doom-modeline-mode 1))
-
 ;; rainbow-delimiters colors the matching brackets
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
